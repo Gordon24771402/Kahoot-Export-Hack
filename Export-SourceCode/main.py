923526f9bef8e0856a1ddc003b5d85ef55f6739e
GK24771402 <63176281+Gordon24771402@users.noreply.github.com>
2021-01-01 21:17:18 +0800

    Update main.py
    1. remove '&nbsp;' - space key - from questions & answers

--- Export-SourceCode/main.py
+++ Export-SourceCode/main.py
@@ -6,25 +6,29 @@ from docx.shared import Pt
 answerURL = 'https://create.kahoot.it/rest/kahoots/{}/card/?includeKahoot=true'.format(input().split('/')[-1])
 data = requests.get(answerURL).json()['kahoot']['questions']
 docQues = Document()
+docAns = Document()
 
 
 for i in range(0, len(data)):
+    text = '{ques}'.format(ques=data[i]['question'])
+    # Remove &nbsp;
+    text = text.replace('&nbsp;', '')
     # Paragraph of Questions
-    paragraph = docQues.add_paragraph(
-        '{ques}'.format(ques=data[i]['question']), style='List Number')
+    paragraph = docQues.add_paragraph(text, style='List Number')
     # Format Options
     paragraph.paragraph_format.line_spacing = 1.0
     paragraph.paragraph_format.space_before = Pt(0)
     paragraph.paragraph_format.space_after = Pt(8)
-    # print('{quesNum}. {ques}'.format(quesNum=i+1, ques=data[i]['question']))
+
     for j in range(0, len(data[i]['choices'])):
+        text = '\t{ansNum}. {ans}'.format(ansNum=list(string.ascii_uppercase)[j], ans=data[i]['choices'][j]['answer'])
+        # Remove &nbsp;
+        text = text.replace('&nbsp;', '')
         # Paragraph of Choices
-        paragraph = docQues.add_paragraph(
-            '\t{ansNum}. {ans}'.format(ansNum=list(string.ascii_uppercase)[j], ans=data[i]['choices'][j]['answer']))
+        paragraph = docQues.add_paragraph(text)
         # Format Options
         paragraph.paragraph_format.line_spacing = 1.0
         paragraph.paragraph_format.space_before = Pt(0)
         paragraph.paragraph_format.space_after = Pt(8)
-        # print('\t{ansNum}. {ans}'.format(ansNum=list(string.ascii_uppercase)[j], ans=data[i]['choices'][j]['answer']))
 
 docQues.save('KahootExport-Question.docx')
