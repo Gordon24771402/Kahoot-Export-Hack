b0d2761c76f18b4835a42de43279fcb2c7ec6794
GK24771402 <63176281+Gordon24771402@users.noreply.github.com>
2021-01-01 20:19:38 +0800

    Update main.py
    1. line_spacing = 1.0

--- Export-SourceCode/main.py
+++ Export-SourceCode/main.py
@@ -6,11 +6,16 @@ answerURL = 'https://create.kahoot.it/rest/kahoots/{}/card/?includeKahoot=true'.
 data = requests.get(answerURL).json()['kahoot']['questions']
 doc = Document()
 
-for i in range(0, len(data)):
-    doc.add_paragraph(
-        '{quesNum}. {ques}'.format(quesNum=i+1, ques=data[i]['question']))
-    for j in range(0, len(data[i]['choices'])):
-        doc.add_paragraph(
-            '\t{ansNum}. {ans}'.format(ansNum=list(string.ascii_uppercase)[j], ans=data[i]['choices'][j]['answer']))
 
-doc.save('KahootExport.docx')
+for i in range(0, len(data)):
+    paragraph = doc.add_paragraph(
+        '{quesNum}. {ques}'.format(quesNum=i+1, ques=data[i]['question']))
+    paragraph.paragraph_format.line_spacing = 1.0
+    # print('{quesNum}. {ques}'.format(quesNum=i+1, ques=data[i]['question']))
+    for j in range(0, len(data[i]['choices'])):
+        paragraph = doc.add_paragraph(
+            '\t{ansNum}. {ans}'.format(ansNum=list(string.ascii_uppercase)[j], ans=data[i]['choices'][j]['answer']))
+        paragraph.paragraph_format.line_spacing = 1.0
+        # print('\t{ansNum}. {ans}'.format(ansNum=list(string.ascii_uppercase)[j], ans=data[i]['choices'][j]['answer']))
+
+doc.save('KahootCapture.docx')
