682370bef22afccdd9013f8437ecbd45abd56cf6
GK24771402 <63176281+Gordon24771402@users.noreply.github.com>
2021-01-07 12:08:58 +0800

    Update main.py
    1. multipleAnswer Supported

--- Export-SourceCode/main.py
+++ Export-SourceCode/main.py
@@ -1,100 +1,102 @@
-import requests
 from docx import Document
 from docx.shared import Pt
-import string
-import platform
-import subprocess
-import os
+from requests import get
+from platform import system
+from os import startfile
+from subprocess import call
+from string import ascii_uppercase
 
-# Create Word Document for Questions and Answers
-docQues = Document()
+# Data Configs
+kahootID = []
+rawData = []
+compData = []
+docQue = Document()
 docAns = Document()
 
-# List Copyright Info and Instruction to Continue
+# Copyright & Instructions
+print('--------------------------------')
 print('Copyright (c) 2020-2021 Hao Kang')
-print('Hit ENTER to Continue Export.\n')
+print('(* Always Hit ENTER to Continue)')
+print('--------------------------------')
+print('Copy and Paste Kahoot URL: ')
 
-# Loop-1: Read Multiple URLs
+# Capture Multiple URLs
 while True:
-
-    # Read URL
-    URL = input('Copy and Paste Kahoot URL: ')
-
-    # Whether to Continue or Break
+    URL = input()
     if URL != '':
-        kahootID = URL.split('/')[-1]
-        answerURL = 'https://create.kahoot.it/rest/kahoots/{}/card/?includeKahoot=true'.format(kahootID)
-        data = requests.get(answerURL).json()['kahoot']['questions']
-
-        # Loop-1-1: Questions
-        for i in range(0, len(data)):
-            text = '{ques}'.format(ques=data[i]['question'])
-            # Remove &nbsp;
-            text = text.replace('&nbsp;', '')
-            # Remove <i> and </i>
-            text = text.replace('<i>', '')
-            text = text.replace('</i>', '')
-            # Remove <b> and </b>
-            text = text.replace('<b>', '')
-            text = text.replace('</b>', '')
-            # Paragraph of Questions
-            paragraph = docQues.add_paragraph(text, style='List Number')
-            # Format Options
-            paragraph.paragraph_format.line_spacing = 1.0
-            paragraph.paragraph_format.space_before = Pt(0)
-            paragraph.paragraph_format.space_after = Pt(8)
-
-            # Loop-1-1-1: Choices
-            for j in range(0, len(data[i]['choices'])):
-                text = '\t{ansNum}. {ans}'.format(ansNum=list(string.ascii_uppercase)[j], ans=data[i]['choices'][j]['answer'])
-                # Remove &nbsp;
-                text = text.replace('&nbsp;', '')
-                # Remove <i> and </i>
-                text = text.replace('<i>', '')
-                text = text.replace('</i>', '')
-                # Remove <b> and </b>
-                text = text.replace('<b>', '')
-                text = text.replace('</b>', '')
-                # Paragraph of Choices
-                paragraph = docQues.add_paragraph(text)
-                # Format Options
-                paragraph.paragraph_format.line_spacing = 1.0
-                paragraph.paragraph_format.space_before = Pt(0)
-                paragraph.paragraph_format.space_after = Pt(8)
-
-            # Loop-1-1-2: Answers
-            for j in range(0, len(data[i]['choices'])):
-                ans = data[i]['choices'][j]['correct']
-                # Determine Correctness
-                if ans:
-                    # Paragraph of Choices
-                    text = list(string.ascii_uppercase)[j]
-                    paragraph = docAns.add_paragraph(text, style='List Number')
-                    # Format Options
-                    paragraph.paragraph_format.line_spacing = 1.0
-                    paragraph.paragraph_format.space_before = Pt(0)
-                    paragraph.paragraph_format.space_after = Pt(8)
+        kahootID.append(URL.split('/')[-1])
     else:
         break
 
-# Save Documents and Notify Each
-docQues.save('KahootExport-Question.docx')
-print('\nSaved: KahootExport-Question.docx')
-docAns.save('KahootExport-Answer.docx')
-print('Saved: KahootExport-Answer.docx')
+# Instructions
+print('--------------------------------')
+print('URL are Captured → Ready to Run')
+print('--------------------------------\n')
 
-# User Experience: Keep Program Run and Open Documents if Necessary
+# Kahoot API
+for a in kahootID:
+    answerURL = 'https://create.kahoot.it/rest/kahoots/{}/card/?includeKahoot=true'.format(a)
+    rawData.extend(get(answerURL).json()['kahoot']['questions'])
+    [compData.append(x) for x in rawData if x not in compData]
+
+
+# Purify Text
+def purify(original):
+    original = original.replace('&nbsp;', '')
+    original = original.replace('<i>', '')
+    original = original.replace('</i>', '')
+    original = original.replace('<b>', '')
+    original = original.replace('</b>', '')
+    return original
+
+
+# Improved Paragraph Format
+def paragraph_format(original):
+    original.paragraph_format.line_spacing = 1.0
+    original.paragraph_format.space_before = Pt(0)
+    original.paragraph_format.space_after = Pt(8)
+
+
+# Loop-i: Capture Questions
+for i in range(0, len(compData)):
+    text = purify('{ques}'.format(ques=compData[i]['question']))
+    paragraphQue = docQue.add_paragraph(text, style='List Number')
+    paragraph_format(paragraphQue)
+
+    # Loop-j: Capture Choices
+    for j in range(0, len(compData[i]['choices'])):
+        text = purify('\t{ansNum}. {ans}'.format(ansNum=list(ascii_uppercase)[j], ans=compData[i]['choices'][j]['answer']))
+        paragraphQue = docQue.add_paragraph(text)
+        paragraph_format(paragraphQue)
+
+    # Capture Answers
+    ans = []
+    [ans.append(k) for k in range(0, len(compData[i]['choices'])) if compData[i]['choices'][k]['correct']]
+    text = ''
+    for x in ans:
+        text += list(ascii_uppercase)[x]
+    paragraphAns = docAns.add_paragraph(text, style='List Number')
+
+# Save Document
+docQue.save('KahootExport-Question.docx')
+docAns.save('KahootExport-Answer.docx')
+print('---------------------------------')
+print('Saved: KahootExport-Question.docx')
+print('Saved: KahootExport-Answer.docx')
+print('---------------------------------')
+
+# Exit and Open
 input('\n(* Hit ENTER to Exit and Open Documents)\n')
 
 # Mac
-if platform.system() == 'Darwin':
-    subprocess.call(('open', 'KahootExport-Question.docx'))
-    subprocess.call(('open', 'KahootExport-Answer.docx'))
+if system() == 'Darwin':
+    call(('open', 'KahootExport-Question.docx'))
+    call(('open', 'KahootExport-Answer.docx'))
 # Windows
-elif platform.system() == 'Windows':
-    os.startfile('KahootExport-Question.docx')
-    os.startfile('KahootExport-Answer.docx')
+elif system() == 'Windows':
+    startfile('KahootExport-Question.docx')
+    startfile('KahootExport-Answer.docx')
 # Linux
 else:
-    subprocess.call(('xdg-open', 'KahootExport-Question.docx'))
-    subprocess.call(('xdg-open', 'KahootExport-Answer.docx'))
+    call(('xdg-open', 'KahootExport-Question.docx'))
+    call(('xdg-open', 'KahootExport-Answer.docx'))
